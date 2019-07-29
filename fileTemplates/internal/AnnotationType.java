843cdc64b1784f99cd7272c27580045c32621db9
Davoleo <davoleo2001@gmail.com>
2019-07-29 11:32:21 +0200

    IU-2019.1.3 <Davoleo@DESKTOP-Q75H7NT Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, project.default.xml, rainbow_brackets.xml, Enum.java, Interface.java, power.mode.ii.xml, Default.xml, androidStudioFirstRun.xml, ignore.xml, find.xml, ui.lnf.xml, log_highlighting.xml, filetypes.xml, androidEditors.xml, plugin_ui.xml, _@user_Darcula.icls, databaseDrivers.xml, github.xml, AnnotationType.java, customization.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, discord.xml, path.macros.xml, vcs.xml, usageView.xml, debugger.xml, setterTemplates.xml, updates.xml

---
+++ fileTemplates/internal/AnnotationType.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/*************************************************
+ * Author: ${USER}
+ * Date / Hour: ${DATE} / ${TIME}
+ * Annotation: ${NAME}
+ * Project: ${PROJECT_NAME}
+ * Copyright - © - Davoleo - ${YEAR}
+ **************************************************/
+
+public @interface ${NAME} {
+}
---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,13 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/*************************************************
+ * Author: ${USER}
+ * Date / Hour: ${DATE} / ${TIME}
+ * Class: ${NAME}
+ * Project: ${PROJECT_NAME}
+ * Copyright - © - Davoleo - ${YEAR}
+ **************************************************/
+
+public class ${NAME} {
+}
