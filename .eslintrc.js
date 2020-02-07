388627cc34bec64327024e3e93d36ffbca1db00e
Tony Brix <tony@brix.ninja>
2020-02-07 12:15:25 -0600

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    jasmine: true,
-    atomtest: true
-  },
-  extends: [
-    'standard'
-  ],
-  globals: {
-    atom: 'readonly',
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {
-    "handle-callback-err": "off",
-    camelcase: "off",
-    "no-warning-comments": "warn"
-  }
-}
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		node: true,
+		jasmine: true,
+		atomtest: true,
+	},
+	extends: [
+		'standard',
+	],
+	globals: {
+		atom: 'readonly',
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
+	rules: {
+		"handle-callback-err": "off",
+		camelcase: "off",
+		"no-warning-comments": "warn",
+		"comma-dangle": ["error", "always-multiline"],
+		semi: ["error", "always"],
+		indent: ["error", "tab"],
+		"no-tabs": ["error", { allowIndentationTabs: true }],
+	},
+};
--- lib/package-manager.coffee
+++ lib/package-manager.coffee
@@ -1,5 +1,5 @@
 # modified from https://raw.githubusercontent.com/atom/settings-view/master/lib/package-manager.coffee
-# removed Client
+# commented out Client on line 8
 
 _ = require 'underscore-plus'
 {BufferedProcess, CompositeDisposable, Emitter} = require 'atom'
