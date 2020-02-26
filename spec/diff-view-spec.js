f7ca011a16ea4dd38f569871167c4dbcd913e762
Tony Brix <tony@brix.ninja>
2020-02-25 22:40:49 -0600

    add diff-view tests

---
+++ spec/diff-view-spec.js
@@ -0,0 +1,161 @@
+const DiffView = require('../lib/diff-view')
+
+function elementsExist (view) {
+	return {
+		error: !!view.element.querySelector('.diff-view-error'),
+		loading: !!view.element.querySelector('.diff-view-loading'),
+		none: !!view.element.querySelector('.diff-view-none'),
+		settings: !!view.element.querySelector('.diff-view-settings'),
+		packages: !!view.element.querySelector('.diff-view-packages'),
+		files: !!view.element.querySelector('.diff-view-files'),
+	}
+}
+
+describe('DiffView', () => {
+	let view
+	beforeEach(() => {
+		view = new DiffView()
+	})
+
+	it('should show loading', () => {
+		expect(elementsExist(view)).toEqual({
+			error: false,
+			loading: true,
+			none: false,
+			settings: false,
+			packages: false,
+			files: false,
+		})
+	})
+
+	it('should show none', async () => {
+		await view.update({ diff: {}, error: null })
+		expect(elementsExist(view)).toEqual({
+			error: false,
+			loading: false,
+			none: true,
+			settings: false,
+			packages: false,
+			files: false,
+		})
+	})
+
+	it('should show error', async () => {
+		await view.update({ diff: null, error: 'test' })
+		expect(elementsExist(view)).toEqual({
+			error: true,
+			loading: false,
+			none: false,
+			settings: false,
+			packages: false,
+			files: false,
+		})
+		const error = view.element.querySelector('.diff-view-error').textContent
+		expect(error).toBe('test')
+	})
+
+	it('should show settings', async () => {
+		await view.update({ diff: { settings: {} } })
+		expect(elementsExist(view)).toEqual({
+			error: false,
+			loading: false,
+			none: false,
+			settings: true,
+			packages: false,
+			files: false,
+		})
+	})
+
+	it('should show packages', async () => {
+		await view.update({ diff: { packages: {} } })
+		expect(elementsExist(view)).toEqual({
+			error: false,
+			loading: false,
+			none: false,
+			settings: false,
+			packages: true,
+			files: false,
+		})
+	})
+
+	it('should show files', async () => {
+		await view.update({ diff: { files: {} } })
+		expect(elementsExist(view)).toEqual({
+			error: false,
+			loading: false,
+			none: false,
+			settings: false,
+			packages: false,
+			files: true,
+		})
+	})
+
+	describe('refresh', () => {
+		it('load diff', async () => {
+			view.syncSettings = {
+				gist: {
+					get () {
+						return {
+							data: {
+								files: 'files',
+							},
+						}
+					},
+				},
+				invalidRes: jasmine.createSpy('invalidRes').and.returnValue(false),
+				getBackupData: jasmine.createSpy('getBackupData').and.returnValue({}),
+				getLocalData: jasmine.createSpy('getLocalData').and.returnValue({}),
+				getDiffData: jasmine.createSpy('getDiffData').and.returnValue({}),
+			}
+			atom.config.set('sync-settings.personalAccessToken', 'pat')
+			atom.config.set('sync-settings.gistId', 'gid')
+			spyOn(view, 'update')
+			await view.refresh()
+			expect(view.syncSettings.getBackupData).toHaveBeenCalled()
+			expect(view.syncSettings.getLocalData).toHaveBeenCalled()
+			expect(view.syncSettings.getDiffData).toHaveBeenCalled()
+			expect(view.update).toHaveBeenCalledWith({ diff: null, error: null })
+			expect(view.update).toHaveBeenCalledWith({ diff: {}, error: null })
+		})
+	})
+
+	describe('buttons', () => {
+		let buttons
+		beforeEach(() => {
+			buttons = view.element.querySelector('.diff-view-buttons')
+		})
+
+		it('calls refresh', async () => {
+			spyOn(view, 'refresh')
+			await view.update({ diff: {} })
+			buttons.querySelector('.refresh').click()
+			expect(view.refresh).toHaveBeenCalled()
+		})
+
+		it('calls restore', async () => {
+			view.syncSettings = {
+				restore: jasmine.createSpy('restore'),
+			}
+			await view.update({ diff: {} })
+			buttons.querySelector('.restore').click()
+			expect(view.syncSettings.restore).toHaveBeenCalled()
+		})
+
+		it('calls backup', async () => {
+			view.syncSettings = {
+				backup: jasmine.createSpy('backup'),
+			}
+			await view.update({ diff: {} })
+			buttons.querySelector('.backup').click()
+			expect(view.syncSettings.backup).toHaveBeenCalled()
+		})
+
+		it('calls viewBackup', () => {
+			view.syncSettings = {
+				viewBackup: jasmine.createSpy('viewBackup'),
+			}
+			buttons.querySelector('.view-backup').click()
+			expect(view.syncSettings.viewBackup).toHaveBeenCalled()
+		})
+	})
+})
