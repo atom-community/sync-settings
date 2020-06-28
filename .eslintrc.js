51043bb34f462705e05a8fe56a2d9fabc15f4bc3
Tony Brix <tony@brix.ninja>
2020-06-28 01:15:07 -0500

    chore(lint): lint .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,12 @@ module.exports = {
 		atomtest: true,
 	},
 	plugins: [
-		'react'
+		'react',
 	],
 	settings: {
 		react: {
-			version: "16"
-		}
+			version: '16',
+		},
 	},
 	extends: [
 		'standard',
@@ -24,19 +24,19 @@ module.exports = {
 	rules: {
 		'no-warning-comments': 'warn',
 		'comma-dangle': ['error', 'always-multiline'],
-		indent: ['error', 'tab', { "SwitchCase": 1 }],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
 		'no-tabs': ['error', { allowIndentationTabs: true }],
 		'no-restricted-globals': [
-            'error',
-            {
-                name: 'fit',
-                message: 'Do not commit focused tests.'
-            },
-            {
-                name: 'fdescribe',
-                message: 'Do not commit focused tests.'
-            },
-        ],
+			'error',
+			{
+				name: 'fit',
+				message: 'Do not commit focused tests.',
+			},
+			{
+				name: 'fdescribe',
+				message: 'Do not commit focused tests.',
+			},
+		],
 		'react/jsx-uses-react': 'error',
 		'react/jsx-uses-vars': 'error',
 		'react/jsx-indent': ['error', 'tab'],
